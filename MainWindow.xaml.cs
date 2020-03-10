2dd0c70d0a972d6c87f546ec288cb91999ac72a7
IanMcT <ian.mctavish@tldsb.on.ca>
2020-03-10 10:11:19 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CardGame2020
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //global variables
+        int[] cards;
+        Random r = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //instantiate
+            cards = new int[52];
+            for (int i = 0; i < cards.Length; i++)
+            {
+                cards[i] = i;
+            }
+
+            string output = "";
+            for (int i = 0; i < cards.Length; i++)
+            {
+                output += cards[i].ToString() + Environment.NewLine;
+            }
+            MessageBox.Show(output);
+            cards = Shuffle(cards);
+            output = "";
+            for (int i = 0; i < cards.Length; i++)
+            {
+                output += cards[i].ToString() + Environment.NewLine;
+            }
+            MessageBox.Show(output);
+        }//end MainWindow
+
+        public int[] Shuffle(int[] c)
+        {
+            for (int i = 0; i < c.Length; i++)
+            {
+                int temp = r.Next(c.Length);
+                int tempValue = c[i];
+                c[i] = c[temp];
+                c[temp] = tempValue;
+            }
+            return c;
+        }
+    }
+}
